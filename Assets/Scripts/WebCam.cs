4adedeb89179e53ee4d9fae72781cd12d2a294b6
Novartemide <zambellanf50@gmail.com>
2026-02-03 21:56:53 +0100

    implementato il secondo slider

--- Assets/Scripts/WebCam.cs
+++ Assets/Scripts/WebCam.cs
@@ -4,37 +4,57 @@ using UnityEngine.Android;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
-public class WebCam : MonoBehaviour{
+public class WebCam : MonoBehaviour
+{
+    // --- VARIABILI INTERNE ---
     private int currentCamIndex = 0;
     private WebCamTexture tex;
     private Grain grainLayer;
+
+    // --- VARIABILI INSPECTOR ---
     [Header("Camera Setup")]
     [SerializeField] private RawImage display;
+
     [Header("Visual Snow Effect")]
     [SerializeField] private PostProcessVolume mainVolume;
+
     [Header("UI Settings")]
     [SerializeField] private GameObject settingsPanel;
-    [SerializeField] private Slider intensitySlider;
     [SerializeField] private GameObject openSettingsButton;
     [SerializeField] private GameObject startButton;
-    [SerializeField] private Slider sizeSlider; 
-    [SerializeField] private Toggle colorToggle; 
+    
+    [Header("Controlli")]
+    [SerializeField] private Slider intensitySlider;
+    [SerializeField] private Slider sizeSlider; // <--- NUOVO SLIDER
 
-
-    void Start(){
-        if(mainVolume != null && mainVolume.profile.TryGetSettings(out grainLayer)){
+    void Start()
+    {
+        // 1. Recuperiamo il Profilo Grain
+        if(mainVolume != null && mainVolume.profile.TryGetSettings(out grainLayer))
+        {
+            // Setup Slider Intensità
             if(intensitySlider != null) {
                 intensitySlider.value = grainLayer.intensity.value;
                 intensitySlider.onValueChanged.AddListener(UpdateIntensity);
             }
+
+            // Setup Slider Grandezza (NUOVO)
+            if(sizeSlider != null) {
+                sizeSlider.value = grainLayer.size.value;
+                sizeSlider.onValueChanged.AddListener(UpdateSize);
+            }
         }
+
+        // 2. Stato Iniziale UI
         if(settingsPanel != null) settingsPanel.SetActive(false);
         if(openSettingsButton != null) openSettingsButton.SetActive(false); 
         if(startButton != null) startButton.SetActive(true);
     }
 
+    // --- FUNZIONI UI (Pubbliche) ---
 
-    public void OnStartPressed(){
+    public void OnStartPressed()
+    {
         #if UNITY_ANDROID
             if (!Permission.HasUserAuthorizedPermission(Permission.Camera)){
                 Permission.RequestUserPermission(Permission.Camera);
@@ -43,36 +63,50 @@ public class WebCam : MonoBehaviour{
 
         if(startButton != null) startButton.SetActive(false);
         if(openSettingsButton != null) openSettingsButton.SetActive(true);
-        
         StartStopCam_Clicked();
     }
 
-    public void OpenSettings(){
+    public void UpdateIntensity(float value)
+    {
+        if(grainLayer != null) grainLayer.intensity.value = value;
+    }
+
+    // NUOVA FUNZIONE PER LA GRANDEZZA
+    public void UpdateSize(float value)
+    {
+        if(grainLayer != null) grainLayer.size.value = value;
+    }
+
+    public void OpenSettings()
+    {
         settingsPanel.SetActive(true);       
         openSettingsButton.SetActive(false); 
     }
 
-    public void CloseSettings(){
+    public void CloseSettings()
+    {
         settingsPanel.SetActive(false);      
         openSettingsButton.SetActive(true);  
     }
 
-    public void SwapCam_Clicked(){
-        if (WebCamTexture.devices.Length > 0){
+    public void SwapCam_Clicked()
+    {
+        if (WebCamTexture.devices.Length > 0)
+        {
             currentCamIndex++;
             currentCamIndex %= WebCamTexture.devices.Length;
-            if (tex != null){
-                StopWebCam();
-                StartStopCam_Clicked();
-            }
+            if (tex != null) { StopWebCam(); StartStopCam_Clicked(); }
         }
     }
-    
-    public void StartStopCam_Clicked(){
-        if (tex != null){
+
+    public void StartStopCam_Clicked()
+    {
+        if (tex != null)
+        {
             StopWebCam();
         }
-        else{
+        else
+        {
             if (WebCamTexture.devices.Length > 0)
             {
                 WebCamDevice device = WebCamTexture.devices[currentCamIndex];
@@ -84,11 +118,8 @@ public class WebCam : MonoBehaviour{
         }
     }
 
-    public void UpdateIntensity(float value){
-        if(grainLayer != null) grainLayer.intensity.value = value;
-    }
-
-    private void StopWebCam(){
+    private void StopWebCam()
+    {
         display.texture = null;
         if(tex != null) tex.Stop();
         tex = null;
